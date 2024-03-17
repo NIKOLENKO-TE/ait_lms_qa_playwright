89d41f3e2570886beb78d7ad20f3aa76c991b0f3
NIKOLENKO-TE <NIKOLENKO-TE@users.noreply.github.com>
2024-03-17 16:59:51 +0000

    deploy: daee26e6e7df4456444a33b727457e4723052c93

--- .batch/login_users_FIREFOX_HEADED — копия.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0..\"
-echo ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-echo 			Start allcom_qa tests in Chrome [ABOUTUS, HEADLESS MODE]		
-echo ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-call gradlew clean login_users -PbrowserType=CHROME -Pheadless=false -Pdevtools=true -Pslowdown=0 -Pallure_report=true
-
-pause
