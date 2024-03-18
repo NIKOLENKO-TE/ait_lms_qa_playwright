2205dff8e2503550a990c2fed11257111d506674
NIKOLENKO-TE <NIKOLENKO-TE@users.noreply.github.com>
2024-03-18 17:13:44 +0000

    deploy: 251abf65b6e7aaf9d6c400ec78a714e3710f722d

--- src/main/java/lms_pages/UI/LessonsPage.java
+++
@@ -1,7 +0,0 @@
-package lms_pages.UI;
-
-public class LessonsPage {
-    public static String lessonsPageURL() {
-        return HomePage.homePageURL() + "student-cabinet/lessons";
-    }
-}
--- src/main/java/lms_pages/UI/StudentCabinet.java
+++
@@ -1,7 +0,0 @@
-package lms_pages.UI;
-
-public class StudentCabinet {
-    public static String studentCabinetURL() {
-        return "https://ait-lms.de/student/cabinet"; // TODO: change to the correct URL
-    }
-}
